e214392cc06ef38e50c408d0c1cf997723d3ae9f
Scott Cabot <iam@scott.cab>
2020-08-29 22:03:58 +0930

    Added GLTF Loader & cube demo.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import * as THREE from 'three';
 import { WEBGL } from 'three/examples/jsm/WebGL';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 
 var scene;
 var camera;
 
 var renderer;
-var cube;
 
 //#region  canvas-resize
 let desiredAspectRatio = 1.45;
@@ -31,36 +31,36 @@ function init() {
     renderer = new THREE.WebGLRenderer();
     document.body.appendChild(renderer.domElement);
 
-    // This is absolutely required (right now) but we don't want it to be
-    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-    camera.position.z = 5;
-
-    // This stuff is super prescriptive and difficult to maintain!
-    var geometry = new THREE.BoxGeometry();
-    var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-    cube = new THREE.Mesh(geometry, material);
-    scene.add(cube);
-
     // Some nice to haves for resize and lighting (you can ignore this if you're watching the demo)
+    scene.background = new THREE.Color('rgb(115, 137, 174)');
     renderer.physicallyCorrectLights = true;
     renderer.setPixelRatio(window.devicePixelRatio);
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    initialCameraFOV = camera.fov;    
+    renderer.setSize(window.innerWidth, window.innerHeight);  
+    
+    // Import the loader
+    var loader = new GLTFLoader();
 
-    // Make the camera somewhat responsive (it's only a demo!)
-    window.addEventListener('resize', onWindowResize, false);
-    onWindowResize();
+    // Load the file
+    loader.load('models/cubes.gltf', (gltf) => {
 
-    render();
+        // Use the blender camera for our scene (nice no more creating one!)
+        camera = gltf.cameras[0];
+        initialCameraFOV = camera.fov;
+
+        // Add the whole blender scene to the three.js scene!
+        scene.add(gltf.scene);
+
+        // Make the camera somewhat responsive (it's only a demo!)
+        window.addEventListener('resize', onWindowResize, false);
+        onWindowResize();
+
+        render();
+    });
 }
 
 function render() {
+    // Our render function is so tidy!!!
     requestAnimationFrame(render);
-
-    // This stuff is super prescriptive and difficult to maintain!
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
-
     renderer.render(scene, camera);
 };
 
