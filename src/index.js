29c1b2f949917551be36e3485d371eef29714959
Scott Cabot <iam@scott.cab>
2020-08-29 20:45:49 +0930

    Tidied demo code and added WebGL checking.

--- src/index.js
+++ src/index.js
@@ -1,26 +1,41 @@
 import * as THREE from 'three';
+import { WEBGL } from 'three/examples/jsm/WebGL';
 
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+var scene;
+var camera;
 
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+var renderer;
+var cube;
 
-var geometry = new THREE.BoxGeometry();
-var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-var cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+const init = function() {
+    scene = new THREE.Scene();
+    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    
+    renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+    
+    var geometry = new THREE.BoxGeometry();
+    var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
+    
+    camera.position.z = 5;
+    animate();
+}
 
-camera.position.z = 5;
-
-var animate = function () {
-    requestAnimationFrame( animate );
+var animate = function() {
+    requestAnimationFrame(animate);
 
     cube.rotation.x += 0.01;
     cube.rotation.y += 0.01;
 
-    renderer.render( scene, camera );
+    renderer.render(scene, camera);
 };
 
-animate();
+if (WEBGL.isWebGLAvailable()) {
+	init();
+} else {
+    var warning = WEBGL.getWebGLErrorMessage();
+	document.getElementById('container').appendChild(warning);
+}
