2d12f62697efe565431e4bff6f78e98eec3e1da2
Scott Cabot <iam@scott.cab>
2020-08-29 20:22:49 +0930

    Scaffolded basic webpack project.

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  mode: 'development',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  watchOptions: {
+    aggregateTimeout: 200,
+    ignored: ['/node_modules/**', 'dist/**']
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000
+  }
+};
